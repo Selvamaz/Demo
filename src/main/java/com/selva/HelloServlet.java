04d7c0ba63d682ad55cbdb3236ca40e251bc3a22
Selvamaz <selvarajumup@gmail.com>
2024-09-18 12:21:49 +0530

    V 0.1
    First Push

---
+++ src/main/java/com/selva/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.selva;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hi this is selva!!!</h1>");
+    }
+}
